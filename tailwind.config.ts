6bc1bf34e5c258dbfb3b1d7a755bbfecbe49179c
double fantasy <cahyawibawa.dev@gmail.com>
2024-05-02 20:37:07 +0700

    feat: new styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,12 +78,32 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'ripple': {
+          "50%": {
+            transform: "scale(var(--scale, 1.25))",
+          },
+        },
+        "progress-bar": {
+          'from': { width: '0' },
+          'to': { width: '100%' },
+        },
+        fade: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeInOut: {
+          '0%, 100%': { opacity: '0' },
+          '20%, 80%': { opacity: '1' },
+        },
       },
       animation: {
         'moveFromRight': "moveFromRight 1000ms linear alternate infinite",
         'moveToRight': "moveToRight 1000ms linear alternate infinite",
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'ripple': "ripple var(--duration, 1.5s) ease calc(var(--i, 0)*0.1s) infinite",
+        "progress-bar": "progress-bar linear ease-in-out",
+        
       },
     },
   },
