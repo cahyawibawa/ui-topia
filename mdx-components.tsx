439e4a147c3fd75d23133c53453f0b5354d34c32
double fantasy <cahyawibawa.dev@gmail.com>
2024-05-16 16:25:34 +0700

    new components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -6,6 +6,7 @@ import {
   Pre,
   type CodeBlockProps,
 } from 'fumadocs-ui/components/codeblock'
+import { File, Files, Folder } from 'fumadocs-ui/components/files'
 import { ImageZoom } from 'fumadocs-ui/components/image-zoom'
 import { Step, Steps } from 'fumadocs-ui/components/steps'
 import { Tab, Tabs } from 'fumadocs-ui/components/tabs'
@@ -27,6 +28,9 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     HomeIcon,
     Accordion,
     Accordions,
+    Files,
+    File,
+    Folder,
     Tabs,
     Tab,
     Callout,
@@ -62,5 +66,102 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     ),
     blockquote: (props) => <Callout>{props.children}</Callout>,
     ...components,
+    h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h1
+        className={cn(
+          'mt-2 scroll-m-20 font-heading text-4xl font-bold',
+          className
+        )}
+        {...props}
+      />
+    ),
+    h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h2
+        className={cn(
+          'mt-12 scroll-m-20 border-b pb-2 font-heading text-2xl font-semibold tracking-tight first:mt-0',
+          className
+        )}
+        {...props}
+      />
+    ),
+    h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h3
+        className={cn(
+          'mt-8 scroll-m-20 font-heading text-xl font-semibold tracking-tight',
+          className
+        )}
+        {...props}
+      />
+    ),
+    h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h4
+        className={cn(
+          'mt-8 scroll-m-20 font-heading text-lg font-semibold tracking-tight',
+          className
+        )}
+        {...props}
+      />
+    ),
+    h5: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h5
+        className={cn(
+          'mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
+          className
+        )}
+        {...props}
+      />
+    ),
+    h6: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h6
+        className={cn(
+          'mt-8 scroll-m-20 text-base font-semibold tracking-tight',
+          className
+        )}
+        {...props}
+      />
+    ),
+    a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
+      <a
+        className={cn('font-medium underline underline-offset-4', className)}
+        {...props}
+      />
+    ),
+    p: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLParagraphElement>) => (
+      <p
+        className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}
+        {...props}
+      />
+    ),
+    ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
+      <ul className={cn('my-6 ml-6 list-disc', className)} {...props} />
+    ),
+    ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
+      <ol className={cn('my-6 ml-6 list-decimal', className)} {...props} />
+    ),
+    li: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+      <li className={cn('mt-2', className)} {...props} />
+    ),
+    img: ({
+      className,
+      alt,
+      ...props
+    }: React.ImgHTMLAttributes<HTMLImageElement>) => (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img className={cn('rounded-md', className)} alt={alt} {...props} />
+    ),
+    hr: ({ ...props }: React.HTMLAttributes<HTMLHRElement>) => (
+      <hr className="my-4 md:my-8" {...props} />
+    ),
+    table: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableElement>) => (
+      <div className="my-6 w-full overflow-y-auto">
+        <table className={cn('w-full', className)} {...props} />
+      </div>
+    ),
   }
 }
