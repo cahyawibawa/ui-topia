05785c8ab1720f5e3f54a168474c607d8d5c8b58
double fantasy <cahyawibawa.dev@gmail.com>
2024-06-12 15:59:23 +0700

    feat: add rehype-katex

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,10 +1,63 @@
-import createMDX from 'fumadocs-mdx/config';
-
-const withMDX = createMDX();
+// @ts-check
+import { rehypeCodeDefaultOptions } from 'fumadocs-core/mdx-plugins'
+import createMDX from 'fumadocs-mdx/config'
+import { transformerTwoslash } from 'fumadocs-twoslash'
+import rehypeKatex from 'rehype-katex'
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-};
+  eslint: {
+    // Replaced by root workspace command
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.module.noParse = [/typescript\/lib\/typescript.js/]
 
-export default withMDX(config);
+    return config
+  },
+}
+
+const withMDX = createMDX({
+  buildSearchIndex: {
+    filter: (v) => {
+      return !v.match(/.+\.model\.mdx/)
+    },
+  },
+  mdxOptions: {
+    rehypeCodeOptions: {
+      themes: {
+        light: 'catppuccin-latte',
+        dark: 'catppuccin-mocha',
+      },
+      transformers: [
+        // @ts-ignore
+        ...rehypeCodeDefaultOptions.transformers,
+        transformerTwoslash(),
+        {
+          name: 'fumadocs:remove-escape',
+          code(element) {
+            element.children.forEach((line) => {
+              if (line.type !== 'element') return
+
+              line.children.forEach((child) => {
+                if (child.type !== 'element') return
+                const textNode = child.children[0]
+                if (!textNode || textNode.type !== 'text') return
+
+                textNode.value = textNode.value.replace(/\[\\!code/g, '[!code')
+              })
+            })
+
+            return element
+          },
+        },
+      ],
+    },
+    lastModifiedTime: 'git',
+    remarkPlugins: [],
+    rehypePlugins: (v) => [rehypeKatex, ...v],
+  },
+})
+
+export default withMDX(config)
