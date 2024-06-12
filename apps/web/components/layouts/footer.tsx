4b290353141547eee4ee62ee8aac25b2357d11fc
double fantasy <cahyawibawa.dev@gmail.com>
2024-06-12 16:01:48 +0700

    fix: footer

--- apps/web/components/layouts/footer.tsx
+++ apps/web/components/layouts/footer.tsx
@@ -1,9 +1,11 @@
+import { siteConfig } from '@/config/site'
+
 export function Footer() {
   return (
     <footer className="py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-muted-foreground text-balance text-center text-sm leading-loose md:text-left">
-          {/* Built by{' '}
+          Built by{' '}
           <a
             href={siteConfig.links.portfolio}
             target="_blank"
@@ -21,7 +23,7 @@ export function Footer() {
           >
             GitHub
           </a>
-          . */}
+          .
         </p>
       </div>
     </footer>
