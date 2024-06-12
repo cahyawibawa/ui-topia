ddb0d04ef3d6f648064ebd3ed700e4670c88695d
double fantasy <cahyawibawa.dev@gmail.com>
2024-06-12 16:02:20 +0700

    fix: eslint

--- apps/web/hooks/use-hero-selection.tsx
+++ apps/web/hooks/use-hero-selection.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { create } from 'zustand'
 
 interface useHeroStore {
