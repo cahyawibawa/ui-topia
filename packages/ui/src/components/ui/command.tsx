a1395c36ee86eeb1a5e2901ab5881dc897df7e09
double fantasy <cahyawibawa.dev@gmail.com>
2024-06-24 23:57:46 +0700

    feat: rm ui

--- packages/ui/src/components/ui/command.tsx
+++ packages/ui/src/components/ui/command.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { type DialogProps } from '@radix-ui/react-dialog'
+// import { type DialogProps } from '@radix-ui/react-dialog'
 import { Command as CommandPrimitive } from 'cmdk'
 import { Search } from 'lucide-react'
 import * as React from 'react'
 
 import { cn } from '../../lib/utils'
-import { Dialog, DialogContent } from './dialog'
+// import { Dialog, DialogContent } from './dialog'
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
@@ -23,19 +23,19 @@ const Command = React.forwardRef<
 ))
 Command.displayName = CommandPrimitive.displayName
 
-interface CommandDialogProps extends DialogProps {}
+// interface CommandDialogProps extends DialogProps {}
 
-const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
-  return (
-    <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg">
-        <Command className="[&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
-          {children}
-        </Command>
-      </DialogContent>
-    </Dialog>
-  )
-}
+// const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
+//   return (
+//     // <Dialog {...props}>
+//     //   <DialogContent className="overflow-hidden p-0 shadow-lg">
+//     //     <Command className="[&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+//     //       {children}
+//     //     </Command>
+//     //   </DialogContent>
+//     // </Dialog>
+//   )
+// }
 
 const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
@@ -144,7 +144,7 @@ CommandShortcut.displayName = 'CommandShortcut'
 
 export {
   Command,
-  CommandDialog,
+  // CommandDialog,
   CommandEmpty,
   CommandGroup,
   CommandInput,
