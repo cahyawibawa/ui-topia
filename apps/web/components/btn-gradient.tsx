7d20ca1fc3e9481c286465a3a0c9ec4c6d5e7b8e
double fantasy <cahyawibawa.dev@gmail.com>
2024-06-25 00:08:13 +0700

    fix: add link

--- apps/web/components/btn-gradient.tsx
+++ apps/web/components/btn-gradient.tsx
@@ -1,14 +1,17 @@
 'use client'
+import Link from 'next/link'
 import { HoverBorderGradient } from './hover-border-gradient'
 
 export function ButtonGradientDemo() {
   return (
-    <HoverBorderGradient
-      containerClassName="rounded-full"
-      as="button"
-      className="flex items-center space-x-2 bg-white text-black dark:bg-black dark:text-white"
-    >
-      <span>Get Started</span>
-    </HoverBorderGradient>
+    <Link href="/docs">
+      <HoverBorderGradient
+        containerClassName="rounded-full"
+        as="button"
+        className="flex items-center space-x-2 bg-white text-black dark:bg-black dark:text-white"
+      >
+        <span>Get Started</span>
+      </HoverBorderGradient>
+    </Link>
   )
 }
