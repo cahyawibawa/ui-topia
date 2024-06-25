be2a22e1af7ac30a44c37779ef6ba6ba23bc58b2
double fantasy <cahyawibawa.dev@gmail.com>
2024-06-25 16:46:54 +0700

    feat: mdx components

--- apps/web/mdx-components.tsx
+++ apps/web/mdx-components.tsx
@@ -1,5 +1,10 @@
 import { Accordion, Accordions } from 'fumadocs-ui/components/accordion'
 import { Callout } from 'fumadocs-ui/components/callout'
+import {
+  CodeBlock,
+  Pre,
+  type CodeBlockProps,
+} from 'fumadocs-ui/components/codeblock'
 import { File, Files, Folder } from 'fumadocs-ui/components/files'
 import { ImageZoom } from 'fumadocs-ui/components/image-zoom'
 import { Step, Steps } from 'fumadocs-ui/components/steps'
@@ -15,8 +20,6 @@ import { ComponentPreview } from './components/component-preview'
 import { extractSourceCode } from './lib/extract-source'
 
 import { cn } from './lib/utils'
-
-// import { CodeBlock, CodeBlockProps, Pre } from './components/code-block'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
@@ -37,107 +40,6 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     ImageZoom,
     Steps,
     Step,
-    h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-      <h1
-        className={cn(
-          'font-heading mt-2 scroll-m-20 text-4xl font-bold',
-          className
-        )}
-        {...props}
-      />
-    ),
-    h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-      <h2
-        className={cn(
-          'font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0',
-          className
-        )}
-        {...props}
-      />
-    ),
-    h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-      <h3
-        className={cn(
-          'font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight',
-          className
-        )}
-        {...props}
-      />
-    ),
-    h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-      <h4
-        className={cn(
-          'font-heading mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
-          className
-        )}
-        {...props}
-      />
-    ),
-    h5: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-      <h5
-        className={cn(
-          'mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
-          className
-        )}
-        {...props}
-      />
-    ),
-    h6: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-      <h6
-        className={cn(
-          'mt-8 scroll-m-20 text-base font-semibold tracking-tight',
-          className
-        )}
-        {...props}
-      />
-    ),
-    a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
-      <a
-        className={cn('font-medium underline underline-offset-4', className)}
-        {...props}
-      />
-    ),
-    p: ({
-      className,
-      ...props
-    }: React.HTMLAttributes<HTMLParagraphElement>) => (
-      <p
-        className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}
-        {...props}
-      />
-    ),
-    ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
-      <ul
-        className={cn('not-prose my-6 ml-6 list-disc', className)}
-        {...props}
-      />
-    ),
-    ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
-      <ol className={cn('my-6 ml-6 list-decimal', className)} {...props} />
-    ),
-    li: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-      <li className={cn('mt-2', className)} {...props} />
-    ),
-    blockquote: (props) => <Callout>{props.children}</Callout>,
-    img: ({
-      className,
-      alt,
-      ...props
-    }: React.ImgHTMLAttributes<HTMLImageElement>) => (
-      // eslint-disable-next-line @next/next/no-img-element
-      <img className={cn('rounded-md', className)} alt={alt} {...props} />
-    ),
-    hr: ({ ...props }: React.HTMLAttributes<HTMLHRElement>) => (
-      <hr className="my-4 md:my-8" {...props} />
-    ),
-    table: ({
-      className,
-      ...props
-    }: React.HTMLAttributes<HTMLTableElement>) => (
-      <div className="my-6 w-full overflow-y-auto">
-        <table className={cn('w-full', className)} {...props} />
-      </div>
-    ),
     ComponentBase: ({ name, ...props }: { name: string }) => {
       const code = extractSourceCode(name)
       return <ComponentBase name={name} code={code} {...props} />
@@ -145,12 +47,11 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     ComponentPreview: ({ name, ...props }) => (
       <ComponentPreview name={name} code={extractSourceCode(name)} {...props} />
     ),
-
-    // pre: ({ title, className, icon, allowCopy, ...props }: CodeBlockProps) => (
-    //   <CodeBlock title={title} icon={icon} allowCopy={allowCopy}>
-    //     <Pre className={cn('max-h-[400px]', className)} {...props} />
-    //   </CodeBlock>
-    // ),
+    pre: ({ title, className, icon, allowCopy, ...props }: CodeBlockProps) => (
+      <CodeBlock title={title} icon={icon} allowCopy={allowCopy}>
+        <Pre className={cn('max-h-[400px]', className)} {...props} />
+      </CodeBlock>
+    ),
     InstallTabs: ({
       items,
       children,
@@ -162,6 +63,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         {children}
       </Tabs>
     ),
+    blockquote: (props) => <Callout>{props.children}</Callout>,
     ...components,
   }
 }
