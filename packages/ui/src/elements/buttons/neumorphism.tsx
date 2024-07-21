f21ca90a9d4c3e0426770206fa66e27e7585ed71
double fantasy <cahyawibawa.dev@gmail.com>
2024-07-21 14:16:47 +0700

    fix: icons

--- packages/ui/src/elements/buttons/neumorphism.tsx
+++ packages/ui/src/elements/buttons/neumorphism.tsx
@@ -1,4 +1,4 @@
-import { PlusIcon } from "@radix-ui/react-icons";
+import { PlusIcon } from '@radix-ui/react-icons'
 
 export function Neumorphism() {
   return (
@@ -6,11 +6,11 @@ export function Neumorphism() {
       <button className="group rounded-[12px] bg-gradient-to-b from-white to-stone-200/40 p-[4px] shadow-[0_1px_3px_rgba(0,0,0,0.5)] active:scale-[0.995] active:shadow-[0_0px_1px_rgba(0,0,0,0.5)]">
         <div className="rounded-[8px] bg-gradient-to-b from-stone-200/40 to-white/80 p-2">
           <div className="flex items-center gap-2">
-            <PlusIcon className="size-5" />
+            <PlusIcon className="size-5 text-stone-400" />
             <span className="font-semibold text-stone-400">Click Me</span>
           </div>
         </div>
       </button>
     </div>
-  );
+  )
 }
