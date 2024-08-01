84590d4ea5c13c306e0b4614064163cab5e856c1
double fantasy <cahyawibawa.dev@gmail.com>
2024-08-01 15:35:12 +0700

    feat: add variant

--- apps/web/components/ui/button.tsx
+++ apps/web/components/ui/button.tsx
@@ -18,9 +18,10 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        bw: 'bg-black text-white dark:bg-white dark:text-black',
       },
       size: {
-        default: 'h-9 px-4 py-2',
+        default: 'h-9 px-4 py-2 text-xs',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-4 py-2',
         icon: 'size-9',
