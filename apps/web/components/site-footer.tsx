9ef332c63ab44e52d3a6586c4b76342ca43587aa
double fantasy <cahyawibawa.dev@gmail.com>
2024-08-01 15:38:40 +0700

    feat: new footer

--- apps/web/components/site-footer.tsx
+++ apps/web/components/site-footer.tsx
@@ -1,29 +1,23 @@
 import { siteConfig } from '@/config/site'
 
 export function SiteFooter() {
+  const currentYear = new Date().getFullYear()
   return (
     <footer className="py-6 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-        <p className="text-muted-foreground text-balance text-center text-sm leading-loose md:text-left">
-          Built by{' '}
-          <a
-            href={siteConfig.links.portfolio}
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            Cahya
-          </a>
-          . The source code is available on{' '}
-          <a
-            href={siteConfig.links.github}
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            GitHub
-          </a>
-          .
+      <div className="flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+        <p className="font-redaction text-muted-foreground flex w-full justify-between text-balance text-center text-sm leading-loose md:text-left">
+          <span>Built with Next.js</span>
+          <span className="flex items-center space-x-1">
+            <a
+              href={siteConfig.links.portfolio}
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium"
+            >
+              Cahya Wibawa
+            </a>
+            <span>• {currentYear}</span>
+          </span>
         </p>
       </div>
     </footer>
