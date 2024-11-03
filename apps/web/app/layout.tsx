acda9c9d54187dd0088f26c57948da66dc8e6797
double fantasy <cahyawibawa.dev@gmail.com>
2024-11-03 18:15:56 +0700

    shared metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,61 +1,30 @@
 import { Toaster } from "@/components/sonner";
-import { siteConfig } from "@/config/site";
 import { Analytics } from "@vercel/analytics/react";
 import { RootProvider } from "fumadocs-ui/provider";
-import "fumadocs-ui/twoslash.css";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 import type { Metadata, Viewport } from "next";
-import "@/styles/globals.css"
+import "@/styles/globals.css";
 import { fontRedaction } from "@/lib/fonts";
+import {
+  defaultMetadata,
+  ogMetadata,
+  twitterMetadata,
+} from "@/lib/metadata/shared-metadata";
 import { cn } from "@/lib/utils";
 
+
 export const metadata: Metadata = {
-  metadataBase: new URL(siteConfig.url),
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  keywords: [
-    "Next.js",
-    "React",
-    "Tailwind CSS",
-    "UI Library",
-    "UI Kit",
-    "UI Components",
-    "UI Elements",
-    "Open Source",
-    "shadcn/ui",
-  ],
-  authors: [
-    {
-      name: "cahya wibawa",
-      url: "https://git.new/cahya",
-    },
-  ],
-  creator: "cahya wibawa",
+  ...defaultMetadata,
   openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
+    ...ogMetadata,
   },
   twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/opengraph-image.png`],
-    creator: "@kyuotaka",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    ...twitterMetadata,
   },
 };
+
+
 export const viewport: Viewport = {
   colorScheme: "dark light",
   themeColor: [
@@ -69,10 +38,16 @@ interface RootLayoutProps {
 }
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" suppressHydrationWarning className={cn(GeistSans.variable, GeistMono.variable, fontRedaction.variable)}>
-      <body
-        className="min-h-screen bg-background font-sans antialiased"
-      >
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(
+        GeistSans.variable,
+        GeistMono.variable,
+        fontRedaction.variable,
+      )}
+    >
+      <body className="min-h-screen bg-background font-sans antialiased">
         <RootProvider>
           {children}
           <Analytics />
--- apps/web/config/site.ts
+++ apps/web/config/site.ts
@@ -1,11 +1,21 @@
 export const siteConfig = {
   name: "ui/topia",
+  creator: "@kyuotaka",
   url: process.env.NEXT_PUBLIC_SITE_URL ?? "https://uitopia.vercel.app",
   ogImage: "https://uitopia.vercel.app/opengraph-image.png",
   description:
-    "An open source project that helps you build beautiful websites with simply copy and paste. Crafted with Tailwind CSS.",
-  blockInfos:
-    "Discover trending UI components ready for instant use. Browse, copy, paste, and elevate your project with customizable, accessible designs.",
+    "ui/topia is, a collection of fine UI components built with Framer Motion and Tailwind CSS.",
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "UI Library",
+    "UI Kit",
+    "UI Components",
+    "UI Elements",
+    "Open Source",
+    "shadcn/ui",
+  ],
   links: {
     portfolio: "https://cahyawibawa.dev/",
     github: "https://git.new/ui/topia",
