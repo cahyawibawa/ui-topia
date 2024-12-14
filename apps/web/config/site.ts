2e8b9843b08220601aa666e6c25e9dd17c0d5fd8
double fantasy <cahyawibawa.dev@gmail.com>
2024-12-14 23:18:26 +0700

    update link url

--- apps/web/config/site.ts
+++ apps/web/config/site.ts
@@ -1,10 +1,10 @@
 export const siteConfig = {
   name: "ui/topia",
   creator: "@kyuotaka",
-  url: process.env.NEXT_PUBLIC_SITE_URL ?? "https://uitopia.vercel.app",
+  url: process.env.NEXT_PUBLIC_APP_URL ?? "https://uitopia.vercel.app",
   ogImage: "https://uitopia.vercel.app/opengraph-image.png",
   description:
-    "ui/topia is, a collection of fine UI components built with Framer Motion and Tailwind CSS.",
+    "ui/topia is, a collection of fine UI components built with Motion and Tailwind CSS.",
   keywords: [
     "Next.js",
     "React",
