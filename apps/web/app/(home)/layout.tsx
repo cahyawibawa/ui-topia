b2e27932100033c0308baff97505fb2b38503be9
cahya <cahyawibawa.dev@gmail.com>
2025-03-07 18:36:18 +0700

    style: change container

--- apps/web/app/(home)/layout.tsx
+++ apps/web/app/(home)/layout.tsx
@@ -11,7 +11,6 @@ export default function Layout({
   return (
     <HomeLayout {...baseOptions}>
       {children}
-
       <Footer />
     </HomeLayout>
   );
@@ -20,38 +19,42 @@ export default function Layout({
 function Footer(): React.ReactElement {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="container mt-auto">
-      <div className="mb-8 text-center text-muted-foreground text-sm">
-        more coming soon...
-      </div>
-      <div
-        className="2xl:!px-0 hidden justify-center xl:flex"
-        style={{ padding: "0 11.5vw" }}
-      >
-        <img
-          src="/images/lines-top.svg"
-          alt="separator"
-          className="h-auto object-contain dark:invert"
-        />
-      </div>
-      <div className="flex flex-col items-start justify-between gap-2 py-4">
-        <div className="flex w-full flex-col items-start justify-between sm:flex-row sm:items-start">
-          <div className="mb-2 flex flex-col sm:mb-0">
-            <span className="font-redaction text-muted-foreground text-sm">
-              Built with Next.js
-            </span>
+    <footer className="border-grid border-t py-6 md:py-0">
+      <div className="container-wrapper">
+        <div className="container py-4">
+          {/* <div className="mb-8 text-center text-muted-foreground text-sm">
+            more coming soon...
+          </div> */}
+          {/* <div
+            className="2xl:!px-0 hidden justify-center xl:flex"
+            style={{ padding: "0 11.5vw" }}
+          >
+            <img
+              src="/images/lines-top.svg"
+              alt="separator"
+              className="h-auto object-contain dark:invert"
+            />
+          </div> */}
+          <div className="flex flex-col items-start justify-between gap-2 py-4">
+            <div className="flex w-full flex-col items-start justify-between sm:flex-row sm:items-start">
+              <div className="mb-2 flex flex-col sm:mb-0">
+                <span className="font-redaction text-muted-foreground text-sm">
+                  Built with Next.js
+                </span>
+              </div>
+              <span className="mt-2 flex items-center space-x-1 font-redaction text-muted-foreground text-sm sm:mt-0">
+                <a
+                  href={siteConfig.links.portfolio}
+                  // biome-ignore lint/a11y/noBlankTarget: <explanation>
+                  target="_blank"
+                  className="font-medium no-underline hover:underline"
+                >
+                  Cahya Wibawa
+                </a>
+                <span>• {currentYear}</span>
+              </span>
+            </div>
           </div>
-          <span className="mt-2 flex items-center space-x-1 font-redaction text-muted-foreground text-sm sm:mt-0">
-            <a
-              href={siteConfig.links.portfolio}
-              // biome-ignore lint/a11y/noBlankTarget: <explanation>
-              target="_blank"
-              className="font-medium no-underline hover:underline"
-            >
-              Cahya Wibawa
-            </a>
-            <span>• {currentYear}</span>
-          </span>
         </div>
       </div>
     </footer>
--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -3,17 +3,23 @@ import { ShowcaseComponent } from "@/components/showcase-component";
 import ActionSearchInput from "@/registry/components/action-search-input";
 import FamilyWallets from "@/registry/components/family-wallets-demo";
 import TextLoopDemo from "@/registry/components/text-loop-demo";
+import { Shell } from "@/uitopia/shell";
 
 export default function Home() {
   return (
-    <div className="container relative">
-      <Hero />
-      <ShowcaseComponent
-        name="action-search-input-demo"
-        demo={ActionSearchInput}
-      />
-      <ShowcaseComponent name="family-wallets-demo" demo={FamilyWallets} />
-      <ShowcaseComponent name="text-loop-demo" demo={TextLoopDemo} />
+    <div className="container-wrapper">
+      <div className="container py-4">
+        <Hero />
+        <ShowcaseComponent
+          name="action-search-input-demo"
+          demo={ActionSearchInput}
+        />
+        <ShowcaseComponent name="family-wallets-demo" demo={FamilyWallets} />
+        <ShowcaseComponent name="text-loop-demo" demo={TextLoopDemo} />
+        {/* <div className="mb-8 text-center text-muted-foreground text-sm">
+          more coming soon...
+        </div> */}
+      </div>
     </div>
   );
 }
