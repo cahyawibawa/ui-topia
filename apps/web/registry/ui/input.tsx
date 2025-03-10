7a2b4a26e2e579c3d6206c9b2c00cf58dc509d0c
cahya <cahyawibawa.dev@gmail.com>
2025-03-10 16:04:58 +0700

    feat: update registry, rollback shadcn script

--- apps/web/registry/ui/input.tsx
+++ apps/web/registry/ui/input.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn } from "@/registry/lib/utils";
 import * as React from "react";
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
--- apps/web/registry/ui/stack.tsx
+++ apps/web/registry/ui/stack.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/registry/lib/utils";
 
 type Direction = "row" | "column";
 type ResponsiveDirection = {
