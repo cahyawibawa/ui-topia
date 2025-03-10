6244f25039ce6b6654ca1b88f7477fb2841c5993
Neeraj Dalal <admin@nrjdalal.com>
2025-03-10 18:50:09 +0530

    chore: tweaks to page.tsx action-search-input-demo.json registry.json action-sea... ~ 4 files 8+ 20-
    apps/web/app/(home)/page.tsx
    apps/web/public/r/action-search-input-demo.json
    apps/web/public/r/registry.json
    apps/web/registry/components/action-search-input-demo.tsx

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import { Hero } from "@/components/hero";
 import { ShowcaseComponent } from "@/components/showcase-component";
-import ActionSearchInput from "@/registry/components/action-search-input";
+import ActionSearchInput from "@/registry/components/action-search-input-demo";
 import FamilyWallets from "@/registry/components/family-wallets-demo";
 import TextLoopDemo from "@/registry/components/text-loop-demo";
 
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="container py-4">
         <Hero />
         <ShowcaseComponent
-          name="action-search-input"
+          name="action-search-input-demo"
           demo={ActionSearchInput}
         />
         <ShowcaseComponent name="family-wallets-demo" demo={FamilyWallets} />
