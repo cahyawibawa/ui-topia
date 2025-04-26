1d90f860b9b23c0aa712753d6f8d18e3aa2744ec
cahya <cahyawibawa.dev@gmail.com>
2025-04-26 13:18:08 +0700

    refactor: update command syntax and replace icon

--- apps/web/components/block-viewer.tsx
+++ apps/web/components/block-viewer.tsx
@@ -295,13 +295,13 @@ function BlockViewerToolbar() {
             size="sm"
             onClick={() => {
               copyToClipboard(
-                `bunx --bun shadcn@latest add https://uitopia.xyz/r/${item.name}.json`,
+                `npx shadcn@latest add https://uitopia.xyz/r/${item.name}.json`,
               );
             }}
           >
             {isCopied ? <Check /> : <Terminal />}
             <span className="hidden lg:inline">
-              bunx --bun shadcn@latest add https://uitopia.xyz/r/{item.name}
+              npx shadcn@latest add https://uitopia.xyz/r/{item.name}
               .json
             </span>
           </Button>
@@ -405,27 +405,7 @@ function BlockViewerCode() {
       </div>
       <div className="flex min-w-0 flex-1 flex-col">
         <div className="flex h-12 items-center gap-2 border-b bg-muted px-4 font-medium text-sm dark:border-zinc-700 dark:bg-zinc-900">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1.75"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="icon icon-tabler icons-tabler-outline icon-tabler-brand-react"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M6.306 8.711c-2.602 .723 -4.306 1.926 -4.306 3.289c0 2.21 4.477 4 10 4c.773 0 1.526 -.035 2.248 -.102" />
-            <path d="M17.692 15.289c2.603 -.722 4.308 -1.926 4.308 -3.289c0 -2.21 -4.477 -4 -10 -4c-.773 0 -1.526 .035 -2.25 .102" />
-            <path d="M6.305 15.287c-.676 2.615 -.485 4.693 .695 5.373c1.913 1.105 5.703 -1.877 8.464 -6.66c.387 -.67 .733 -1.339 1.036 -2" />
-            <path d="M17.694 8.716c.677 -2.616 .487 -4.696 -.694 -5.376c-1.913 -1.105 -5.703 1.877 -8.464 6.66c-.387 .67 -.733 1.34 -1.037 2" />
-            <path d="M12 5.424c-1.925 -1.892 -3.82 -2.766 -5 -2.084c-1.913 1.104 -1.226 5.877 1.536 10.66c.386 .67 .793 1.304 1.212 1.896" />
-            <path d="M12 18.574c1.926 1.893 3.821 2.768 5 2.086c1.913 -1.104 1.226 -5.877 -1.536 -10.66c-.375 -.65 -.78 -1.283 -1.212 -1.897" />
-            <path d="M11.5 12.866a1 1 0 1 0 1 -1.732a1 1 0 0 0 -1 1.732z" />
-          </svg>
+          <File className="h-4 w-4" />
           {activeFile}
           {/* <div className="ml-auto flex items-center gap-2">
             <BlockCopyCodeButton />
@@ -515,7 +495,7 @@ function Tree({ item, index }: { item: FileTree; index: number }) {
             }
           >
             <ChevronRight className="h-4 w-4 transition-transform" />
-            <Folder className="h-4 w-4" />
+            <FolderOpen className="h-4 w-4" />
             {item.name}
           </SidebarMenuButton>
         </CollapsibleTrigger>
